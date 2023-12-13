f64d40d2f6419261ce9f889db9b61c68a89954c6
Arhiiiip <rabokony@icloud.com>
2023-12-14 00:21:25 +0300

    add config for SpringBoot and change for server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.0"
 }
+
+apply(plugin = "io.spring.dependency-management")
+
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -10,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }

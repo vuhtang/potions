6c56f14ad0ac9008f041c38d994d933fd30b37c4
Arhiiiip <rabokony@icloud.com>
2024-01-11 16:32:40 +0300

    refactor stylesheets and js code

--- src/main/resources/static/js/user/orderCreateCompletePage.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("toStartPage").addEventListener("click", function() {
-    window.location.href = "http://localhost:8080/user/start";
-});
--- src/main/resources/static/js/user/startPage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("redirectStatusButton").addEventListener("click", function() {
-    window.location.href = "http://localhost:8080/orders/checkStatusPage";
-});
-
-document.getElementById("redirectOrderButton").addEventListener("click", function() {
-    window.location.href = "http://localhost:8080/orders/createFormPage";
-});
